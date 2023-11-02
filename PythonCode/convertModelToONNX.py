5f4c829bb29987c8430d6356b5e41ae327abdf86
BrianHemmat <49881898+BrianHemmat@users.noreply.github.com>
2023-11-02 09:25:05 -0400

    Update convertModelToONNX.py

--- PythonCode/convertModelToONNX.py
+++ PythonCode/convertModelToONNX.py
@@ -8,10 +8,10 @@ model.eval()
 # Convert to ONNX format
 x = torch.empty(1, 1, 98, 50)
 torch.onnx.export(model,
-                      x,
-                      "cmdRecognitionPyTorch.onnx",
-                      export_params=True,
-                      opset_version=9,
-                      do_constant_folding=True,
-                      input_names=['input'],
-                      output_names=['output'])
+                  x,
+                  "cmdRecognitionPyTorch.onnx",
+                  export_params=True,
+                  opset_version=9,
+                  do_constant_folding=True,
+                  input_names=['input'],
+                  output_names=['output'])
