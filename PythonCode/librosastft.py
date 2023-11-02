0d32eab339eff56bdc04336419db114696bfe45e
BrianHemmat <49881898+BrianHemmat@users.noreply.github.com>
2023-11-02 09:27:08 -0400

    Update librosastft.py

--- PythonCode/librosastft.py
+++ PythonCode/librosastft.py
@@ -4,14 +4,21 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Read audio
-sr = 16e3
-samples, sr = librosa.load(filename, sr)
+desired_sr = 16e3
+samples, sr = librosa.load(filename,sr=desired_sr)
 
 # STFT
-stftOut = librosa.stft(samples, n_fft=512, hop_length=160,
-                       win_length=512,window="hann",
+stftOut = librosa.stft(samples,
+                       n_fft=512,
+                       hop_length=160,
+                       win_length=512,
+                       window="hann",
                        center=True)
 
 # ISTFT
-istftOut = librosa.istft(stftOut, n_fft=512, hop_length=160,
-                         win_length=512,window="hann", center=True)
+istftOut = librosa.istft(stftOut,
+                         n_fft=512,
+                         hop_length=160,
+                         win_length=512,
+                         window="hann",
+                         center=True)
