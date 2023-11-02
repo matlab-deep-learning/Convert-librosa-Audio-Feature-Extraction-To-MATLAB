20efb468209f89c634a38a20e218c5d1a861421e
BrianHemmat <49881898+BrianHemmat@users.noreply.github.com>
2023-11-02 09:26:16 -0400

    Update librosamfcc.py

--- PythonCode/librosamfcc.py
+++ PythonCode/librosamfcc.py
@@ -4,11 +4,18 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Read audio
-sr = 16e3
-samples, sr = librosa.load(filename, sr)
+desired_sr = 16e3
+samples, sr = librosa.load(filename,sr=desired_sr)
 
 # MFCC
-mfccOut = librosa.feature.mfcc(samples,sr=sr,n_fft=512, n_mels=50,
-                               hop_length=160,win_length=512,
-                               window="hann", htk=True,power=2,
-                               dct_type=2,lifter=.2)
+mfccOut = librosa.feature.mfcc(y=samples,
+                               sr=sr,
+                               n_fft=512,
+                               n_mels=50,
+                               hop_length=160,
+                               win_length=512,
+                               window="hann",
+                               htk=False,
+                               power=2,
+                               dct_type=2,
+                               lifter=.2)
