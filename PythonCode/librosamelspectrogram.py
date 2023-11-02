8bc6b1843c52aaba96bc0c48b14deab8d86a739a
BrianHemmat <49881898+BrianHemmat@users.noreply.github.com>
2023-11-02 09:25:56 -0400

    Update librosamelspectrogram.py

--- PythonCode/librosamelspectrogram.py
+++ PythonCode/librosamelspectrogram.py
@@ -4,10 +4,18 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Read audio
-sr = 16e3
-samples, sr = librosa.load(filename, sr)
+desired_sr = 16e3
+samples, sr = librosa.load(filename,sr=desired_sr)
 
 # Mel spectrogram
-melSpectrogramOut = librosa.feature.melspectrogram(samples,sr=sr,
-                    n_fft=512,n_mels=50, norm="slaney",htk=True,hop_length=160,
-                    win_length=512,window="hann",center=False,power=2)
+melSpectrogramOut = librosa.feature.melspectrogram(y=samples,
+                                                   sr=sr,
+                                                   n_fft=512,
+                                                   n_mels=50, 
+                                                   norm="slaney",
+                                                   htk=False,
+                                                   hop_length=160,
+                                                   win_length=512,
+                                                   window="hann",
+                                                   center=False,
+                                                   power=2)
