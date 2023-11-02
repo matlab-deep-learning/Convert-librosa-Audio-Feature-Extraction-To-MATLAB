b1a15513f9c3686502393a85d66b6cb4d9d0fe7f
BrianHemmat <49881898+BrianHemmat@users.noreply.github.com>
2023-11-02 09:25:34 -0400

    Update librosamel.py

--- PythonCode/librosamel.py
+++ PythonCode/librosamel.py
@@ -6,5 +6,8 @@ warnings.filterwarnings("ignore")
 sr = 16e3
 
 # Mel Filter bank
-melOut = librosa.filters.mel(sr=sr,n_fft=512, n_mels=50,
-                             norm="slaney", htk=True)
+melOut = librosa.filters.mel(sr=sr,
+                             n_fft=512,
+                             n_mels=50,
+                             norm="slaney",
+                             htk=False)
